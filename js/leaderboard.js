82f39f19fd6be72b7cf8782e984dfb7911673295
Lee Ming Feng <S10219107@connect.np.edu.sg>
2022-02-18 20:08:21 +0800

    Add api js code for restDB for leaderboard Co-authored-by: Leo Yun Tao <LeoYunTao@users.noreply.github.com>

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,50 +1,5 @@
-const APIKEY = "620e41f234fd621565858720";
-
-$("#finish").on("click", function (f) {
-    f.preventDefault();
-
-    let name = $("#name").val();
-    let time = $("#time").val();
-    let score = $("#score").val();
-    let accuracy = $("#accuracy").val();
-
-    //[STEP 3]: get form values when user clicks on send
-    //Adapted from restdb api
-    let jsondata = {
-      "name": name,
-      "time": time,
-      "score": score,
-      "accuracy": accuracy,
-    };
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://fastgame-8ea2.restdb.io/rest/details",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": APIKEY,
-            "cache-control": "no-cache"
-        },
-        "processData": false,
-        "data": JSON.stringify(jsondata),
-        "beforeSend": function(){
-            $("#finish").prop( "disabled", true);
-            $("#???????????????????").trigger("reset");
-            }
-        }
-        
-        $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
-})
-
-function addZero(i) {
-    if (i < 10) {i = "0" + i}
-    return i;
-  }
-
+$("#leaderBoard").hide();
+$("#tableLoading").show();
 getLeaderboard();
 
 let allPlayers = [];
@@ -71,14 +26,13 @@ function getLeaderboard(){
         }
     }
 
+
     $.ajax(settings).done(function (response) {
         for (let i = 0; i < response.length; i++) {
             const player = response[i];
             player.rank = i + 1;
             allPlayers.push(player);
         }
-        // rankList.sort(function(a = rankList[i].score, b = rankList[i++].score){return b - a})
-        //return rankList;
         updateTable(allPlayers);
     });
 }
@@ -86,14 +40,15 @@ function getLeaderboard(){
 function updateTable(rows, limit=100) {
     let content = "";
     for (var i = 0; i < rows.length && i < limit; i++) {
-
         content = `${content}<tr class="fs-5" scope="row"'>
             <td class="fs-5">${rows[i].rank}</td>
             <td id="name" class="fs-5">${rows[i].name}</td>
-            <td id="time" class="fs-5">${rows[i][1]}</td>
+            <td id="time" class="fs-5">${millisecondsToMinutesAndSeconds(rows[i].time)}s</td>
             <td class="fs-5">${rows[i].score}</td>
-            <td class="fs-5">${rows[i].accuracy}%</td>
+            <td class="fs-5">${(rows[i].accuracy * 100).toFixed(2)}%</td>
             </tr>`;
         $("#leaderBoard tbody").html(content);
     }
+    $("#tableLoading").hide();
+    $("#leaderBoard").show();
 }
